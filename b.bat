e9eaf2472a390691af47e40749e2eb7804c5bbb6
iwabuchiken <iwabuchi.k.2010@gmail.com>
2014-07-08 17:48:37 +0900

    m
    J/C

---
+++ b.bat
@@ -0,0 +1,8 @@
+pushd "C:\Program Files\Google\Chrome\Application"
+
+start chrome.exe http://localhost/Learn_AI_Mecab/first/first_utf8.php
+REM start chrome.exe http://benfranklin.chips.jp/cake_apps/VM_Cake/videos
+start chrome.exe "https://mysqladmin.lolipop.jp/pma/index.php?db=LAA0278957-cakevm&table=positions&target=sql.php&server=110&token=cb582dcc1a162a60fddda6c688b049f0"
+start chrome.exe chrome-extension://ohbfkkmpnlpgbbfdflaiikoohbidaikj/app.html
+popd
+
